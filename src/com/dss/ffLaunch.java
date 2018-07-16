685161973e1c8a7d50c46a8bd2abe6de90c768ce
rambej0201 <1503010201@svuca.edu>
2018-07-15 18:55:15 -0700

    Selenium Practice Project

---
+++ src/com/dss/ffLaunch.java
@@ -0,0 +1,15 @@
+package com.dss;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ffLaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+		
+	}
+
+}
